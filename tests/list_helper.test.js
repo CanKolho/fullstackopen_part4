8cbb3983a2d1afea3937ba53456f0aa36e1e287f
Can Kolho <can.kolho@aalto.fi>
2023-09-21 13:13:32 +0300

    4.7*: apufunktioita ja yksikkötestejä, step5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -38,3 +38,23 @@ describe('favorite blog', () => {
   })
 })
 
+describe('most blogs', () => {
+  test('returns the author and count of blogs', () => {
+    const result = listHelper.mostBlogs(listWithMultipleBlogs)
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+})
+
+describe('most likes', () => {
+  test('returns the author and count of likes', () => {
+    const result = listHelper.mostLikes(listWithMultipleBlogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17
+    })
+  })
+})
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,11 +1,11 @@
+const lodash = require("lodash")
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
 const favoriteBlog = (blogs) => {
-  if (blogs.length === 0) {
-    return null
-  }
+  if (blogs.length === 0) return null
 
   const arrOfLikes = blogs.map(blog => blog.likes)
   const mostLikesIdx = arrOfLikes.indexOf(Math.max(...arrOfLikes))
@@ -19,8 +19,43 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authorCountArr = lodash.countBy(blogs, 'author')
+  const entries = Object.entries(authorCountArr)
+  const blogCount = entries.map(entry => entry[1])
+  const mostBlogsIdx = blogCount.indexOf(Math.max(...blogCount))
+  const topAuthor = entries[mostBlogsIdx]
+
+  return {
+    author: topAuthor[0],
+    blogs: topAuthor[1],
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authorsGrouped = lodash.groupBy(blogs, 'author')
+  const keys = Object.keys(authorsGrouped)
+
+  const mostLikesarr = keys.map(key => 
+    lodash.sumBy(authorsGrouped[key], 'likes')
+  )
+
+  const mostLikesIdx = mostLikesarr.indexOf(Math.max(...mostLikesarr))
+
+  return {
+    author: keys[mostLikesIdx],
+    likes: mostLikesarr[mostLikesIdx],
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
