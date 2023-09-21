a900e0802e9641455d1ad8fa82484afe5b5b7aa8
Can Kolho <can.kolho@aalto.fi>
2023-09-21 17:37:48 +0300

    4.10: blogilistan testit, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,11 +29,34 @@ describe('GET to api endpoint /api/blogs', () => {
       const response = await api.get('/api/blogs')
 
       const ids = response.body.map(blog => blog.id)
-      
+
       ids.forEach(id => expect(id).toBeDefined())
     })
 })
   
+describe('POST to api endpoint /api/blogs', () => {
+  test('Adds new blog correctly to database', async () => {
+    const newBlog = {
+      title: 'MongoDB is for storing data',
+      author: 'db management',
+      url: 'www.mongodb.com',
+      likes: 1000
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('MongoDB is for storing data')
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
